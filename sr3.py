02782b5b079f3eb8ad966ecc2d333074604937e8
voidnologo <(none)>
2018-02-05 12:56:31 -0600

    target numbers, offset, modifier

--- sr3.py
+++ sr3.py
@@ -3,6 +3,7 @@ import random
 
 
 custom_commands = {}
+TN_MODIFIER = 0
 
 
 def roll_pool(quantity, rule_of_six=True):
@@ -24,21 +25,38 @@ def straight_roll(quantity):
         yield random.randint(1, 6)
 
 
-def new():
+def new_cmd(args=None):
+    '''
+        Create a new macro
+           command = name of command
+           pool size = number of dice to roll
+           rule-of-six = whether to reroll 6s
+           offset = value to add to total
+        Usage:
+           `{name} [tn]`
+    '''
     command = input('Command: ')
     size = int(input('Pool size: '))
     rule_of_six = False if input('Use Rule of Six? Y/n: ').upper() == 'N' else True
-    custom_commands[command] = dict(size=size, rule_of_six=rule_of_six)
+    o = input('Offset: ')
+    offset = int(o) if o else 0
+    custom_commands[command] = dict(size=size, rule_of_six=rule_of_six, offset=offset)
 
 
-def show_custom():
+def show_custom(args=None):
+    '''
+        Show custom macros
+        Format is {name} | {pool size} | {use rule-of-six} | {offset}
+    '''
+    w = max(len(i) for i in custom_commands.keys()) if custom_commands.keys() else 0
     for c in custom_commands.keys():
         size = custom_commands[c]['size']
         ros = 'Y' if custom_commands[c]["rule_of_six"] else 'N'
-        print(f'{c:>15} > {size:5}\t{ros}')
+        off = custom_commands[c]['offset']
+        print(f'{c:>{w + 3}} > {size:3} | { ros} | {off:+}')
 
 
-def print_pool(pool):
+def print_pool(pool, tn=None, quantity=None, offset=None):
     try:
         acc = accumulate()
         next(acc)
@@ -50,8 +68,20 @@ def print_pool(pool):
         except StopIteration as data:
             counter, total = data.value
     for k in sorted(counter.keys()):
-        print(f'{k:6>}: {counter[k]:,}')
-    print(f'total: {total:,}')
+        print(f'{k:>6}: {counter[k]:,}')
+    total = f'{total}{offset:+}({total+offset:,})' if offset else f'{total:,}'
+    target = (f'({tn[0]}>{int(tn[0]) + TN_MODIFIER})' if TN_MODIFIER else f'({tn[0]})') if tn else ''
+    successes = f'TN{target}: {count_successes(counter, int(tn[0]))}' if tn else ''
+    glitch = f'!! GLITCH !! ' if counter.get(1, 0) >= quantity//2 else ''
+    print(f'{glitch}{successes}{" <> " if successes else ""}total: {total}')
+
+
+def count_successes(pool, tn):
+    s = 0
+    for i in pool:
+        if i >= tn + TN_MODIFIER:
+            s += pool[i]
+    return s
 
 
 def accumulate():
@@ -66,29 +96,50 @@ def accumulate():
     return accumulator, total
 
 
+def set_tn_modifier(mod=None):
+    '''
+        Set modifier to Target Numbers
+        `:mod` will set it to 0
+        `:mod #` will set it to #
+        `:mod ?` will display the current value
+    '''
+    global TN_MODIFIER
+    if mod and mod[0] == '?':
+        print(f'TN Modifier is: {TN_MODIFIER}')
+        return
+    TN_MODIFIER = int(mod[0]) if mod else 0
+    print(f'TN Modifier is: {TN_MODIFIER}')
+
+
+def help(args=None):
+    for opt in options:
+        if options[opt] is not help:
+            print(f'{opt} {options[opt].__doc__}')
+
+
 options = {
-    ':n': new,
+    ':n': new_cmd,
     ':l': show_custom,
+    ':mod': set_tn_modifier,
+    ':help': help,
+    ':?': help
 }
 
 
 while(True):
     inp = input(">> ")
+    cmd, *args = inp.split()
+    if cmd in options.keys():
+        options[cmd](args)
+        continue
+    if cmd in custom_commands.keys():
+        quantity = custom_commands[cmd]['size']
+        pool = roll_pool(quantity, custom_commands[cmd]['rule_of_six'])
+        print_pool(pool, args, quantity=quantity, offset=custom_commands[cmd]['offset'])
+        continue
     try:
-        options[inp]()
-    except KeyError:
-        try:
-            quantity = custom_commands[inp]['size']
-            pool = roll_pool(quantity, custom_commands[inp]['rule_of_six'])
-            print_pool(pool)
-        except KeyError:
-            try:
-                quantity = int(inp)
-                pool = roll_pool(quantity)
-                print_pool(pool)
-            except Exception:
-                print('Invalid input')
-        except Exception:
-            print('Invalid input')
-    except Exception:
-        print('Invalid input')
+        quantity = int(cmd)
+        pool = roll_pool(quantity)
+        print_pool(pool, args, quantity=quantity)
+    except Exception as e:
+        print('Error:', e)
