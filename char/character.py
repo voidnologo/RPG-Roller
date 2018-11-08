1b6f68b38331dead121b41c040a8fe90b99f0526
voidnologo <(none)>
2018-11-07 22:33:06 -0500

    Add Stat dataclass

--- char/character.py
+++ char/character.py
@@ -1,7 +1,23 @@
 from collections import ChainMap
+from dataclasses import dataclass
 import random
 import json
 import sys
+
+
+@dataclass
+class Stat:
+    base: int = 0
+    ros: bool = True
+    offset: int = 0
+
+    def __post_init__(self):
+        self.base = int(self.base) if self.base.isdigit() else 0
+        self.ros = False if self.ros == 'F' else True
+        self.offset = int(self.offset) if self.offset.isdigit() else 0
+
+    def __repr__(self):
+        return f'{self.base};{"T" if self.ros else "F"};{self.offset}'
 
 
 def read_character(file_name):
@@ -9,27 +25,35 @@ def read_character(file_name):
         return json.load(f)
 
 
-def roll(number):
+def lookup_stat(stat):
+    raw = ChainMap(stats['attributes'], stats['skills'], stats['pools']).get(stat, ";;")
+    return Stat(*(raw.split(';')))
+
+
+def roll(number, ros=True):
     for _ in range(number):
         die = random.randint(1, 6)
-        if die == 6:
+        if ros and die == 6:
             die += next(roll(1))
         yield die
 
 
-def roll_stat(stat):
+def roll_stat(query):
     try:
-        number = ChainMap(char['attributes'], char['skills'], char['pools']).get(stat, 0)
-        yield from roll(number)
-    except NameError:
-        print('Failed !!')
+        stat = lookup_stat(query)
+        if stat.offset:
+            yield sum(roll(stat.base, ros=stat.ros)) + stat.offset
+        else:
+            yield from roll(stat.base, ros=stat.ros)
+    except NameError as e:
+        print('Failed !!', e)
         return (_ for _ in [])
 
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
     print(f'Reading character file {file_name}')
-    char = read_character(file_name)
+    stats = read_character(file_name)
     while True:
         cmd = input('>> ')
         print(list(roll_stat(cmd)))
--- char/tests/test_char.py
+++ char/tests/test_char.py
@@ -1,75 +1,81 @@
+from pathlib import Path
 import unittest
 from unittest import mock
 
-import character
+import char.character
 
 
 class CharTests(unittest.TestCase):
 
     def setUp(self):
-        char = {
-            'attributes': {
-                'strength': 5
-            },
-            'skills': {
-                'pistols': 6
-            },
-            'pools': {
-                'combat': 6
-            },
-            'meta': {
-                'perception': 5,
-                'physical_damage': 0,
-                'stun_damage': 4
-            }
-        }
-        setattr(character, 'char', char)
+        fpath = Path(__file__).resolve().parent.joinpath(Path('data', 'tommy_talon.char'))
+        stats = char.character.read_character(fpath)
+        setattr(char.character, 'stats', stats)
 
     def test_roll(self):
-        results = list(character.roll(5))
+        results = list(char.character.roll(5))
         self.assertEqual(5, len(results))
 
     @unittest.mock.patch('random.randint')
     def test_roll_implements_rule_of_six(self, rand):
         rand.side_effect = [6, 3]
-        results = list(character.roll(1))
+        results = list(char.character.roll(1))
         self.assertEqual(results, [9])
 
     def test_can_roll_for_attribute(self):
-        results = character.roll_stat('strength')
+        results = char.character.roll_stat('strength')
         self.assertEqual(5, len(list(results)))
 
     def test_can_roll_for_skills(self):
-        results = character.roll_stat('pistols')
+        results = char.character.roll_stat('pistols')
         self.assertEqual(6, len(list(results)))
 
     def test_can_roll_for_pool(self):
-        results = character.roll_stat('combat')
+        results = char.character.roll_stat('combat')
         self.assertEqual(6, len(list(results)))
 
     def test_returns_empty_list_if_stat_not_found(self):
-        results = character.roll_stat('definitely_not_there')
+        results = char.character.roll_stat('definitely_not_there')
         self.assertEqual(0, len(list(results)))
 
-    def test_can_read_in_character(self):
-        char = character.read_character('data/tommy_talon.char')
-        expected = {
-            'attributes': {
-                'strength': 5
-            },
-            'skills': {
-                'pistols': 6
-            },
-            'pools': {
-                'combat': 6
-            },
-            'meta': {
-                'perception': 5,
-                'physical_damage': 0,
-                'stun_damage': 4
-            }
-        }
-        self.assertEqual(char, expected)
+    # def test_can_read_in_character(self):
+    #     fpath = Path(__file__).resolve().parent.joinpath(Path('data', 'tommy_talon.char'))
+    #     stats = char.character.read_character(fpath)
+    #     expected = {
+    #         "attributes": {
+    #             "strength": 5
+    #         },
+    #         "skills": {
+    #             "pistols": 6
+    #         },
+    #         "pools": {
+    #             "combat": 6,
+    #             "initiative": {
+    #                 "base": 2,
+    #                 "rule_of_six": False,
+    #                 "offset": 9
+    #             }
+    #         },
+    #         "health": {
+    #             "physical_damage": 0,
+    #             "stun_damage": 4
+    #         }
+    #     }
+    #     self.assertEqual(stats, expected)
+
+    @unittest.mock.patch('random.randint')
+    def test_initiative_adds_offset(self, rand):
+        rand.side_effect = [3, 3]
+        results = list(char.character.roll_stat('initiative'))
+        self.assertEqual(1, len(results))
+        self.assertEqual(15, results[0])
+
+    @unittest.mock.patch('random.randint')
+    def test_initiative_does_not_use_rule_of_six(self, rand):
+        rand.side_effect = [6, 3]
+        results = list(char.character.roll_stat('initiative'))
+        self.assertEqual(1, len(results))
+        self.assertEqual(18, results[0])
 
 
 if __name__ == '__main__':
