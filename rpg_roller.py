2b6427792b27be8f44e74f4b98822252c84f0fab
voidnologo <(none)>
2020-10-01 10:03:23 -0400

    enter to reroll prior dice

--- rpg_roller.py
+++ rpg_roller.py
@@ -1,10 +1,14 @@
 import random
 from collections import Counter
 
+prior_inp = None
 
-while(True):
+while True:
     inp = input(">> ")
-    values = inp.split('d')
+    if inp == "":
+        inp = prior_inp
+    prior_inp = inp
+    values = inp.split("d")
     quantity = int(values[0]) if values[0] else 1
     faces = int(values[1])
 
@@ -12,4 +16,4 @@ while(True):
     for _ in range(quantity):
         pool.update({random.randint(1, faces): 1})
 
-    print('{}\ntotal: {:,}'.format(sorted(pool.elements()), sum(pool.elements())))
+    print("{}\ntotal: {:,}".format(sorted(pool.elements()), sum(pool.elements())))
